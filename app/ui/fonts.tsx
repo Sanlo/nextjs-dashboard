cb4c0a06a7b46e2a3af7bcb962bc84f67dcbed5f
Sanlo <sanlozhang@skiff.com>
2024-10-05 16:31:56 +0800

    fonts and images

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+﻿import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
