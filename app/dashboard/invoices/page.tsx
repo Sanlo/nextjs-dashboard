9db9b5e20b4e84528eabd3e65087f480b91655cf
Sanlo <sanlozhang@skiff.com>
2024-10-06 21:33:57 +0800

    pagination with url params

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,8 +4,10 @@ import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import Table from "@/app/ui/invoices/table";
 import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
+import Pagination from "@/app/ui/invoices/pagination";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams?: {
@@ -15,6 +17,8 @@ export default function Page({
 }) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -28,7 +32,9 @@ export default function Page({
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
-      <div className="mt-5 flex w-full justify-between"></div>
+      <div className="mt-5 flex w-full justify-between">
+        <Pagination totalPages={totalPages} />
+      </div>
     </div>
   );
 }
