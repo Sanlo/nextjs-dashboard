4042b6fa7dbc13c39d2b5c61d7e3635a77baff64
Sanlo <sanlozhang@skiff.com>
2024-10-06 16:17:38 +0800

    add Mantine

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
+import "@mantine/core/styles.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={`${inter.className} antialiased`}>
+        <MantineProvider defaultColorScheme="auto">{children}</MantineProvider>
+      </body>
     </html>
   );
 }
