283d84d365203f29dd85bac50f1feeabe8bd9ea6
Sanlo <sanlozhang@skiff.com>
2024-10-06 18:19:53 +0800

    streaming and server component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-// import "@mantine/core/styles.css";
-// import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
@@ -7,10 +7,12 @@ import { inter } from "@/app/ui/fonts";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>{/* <ColorSchemeScript defaultColorScheme="light" /> */}</head>
+      <head>
+        <ColorSchemeScript defaultColorScheme="dark" />
+      </head>
       <body className={`${inter.className} antialiased`}>
-        {/* <MantineProvider>{children}</MantineProvider> */}
-        {children}
+        <MantineProvider>{children}</MantineProvider>
+        {/* {children} */}
       </body>
     </html>
   );
