2f727b24191e010d156487d6836befa67bb9b99c
Sanlo <sanlozhang@skiff.com>
2024-10-07 10:51:04 +0800

    mutating data with create new record

---
+++ app/lib/actions.ts
@@ -0,0 +1,42 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+// validate and prepare the data
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+/**
+ * React Server Action
+ * @param formData FormData
+ */
+export async function createInvoice(formData: FormData) {
+  // extract the data from form and validate
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  // insert the data into db
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // revalidate and redirect
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
