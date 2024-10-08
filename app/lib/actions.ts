f3af2b9cfd426a4ef38af733e6fe216dbd163777
Sanlo <sanlozhang@skiff.com>
2024-10-08 22:52:20 +0800

    add authentication support

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
 import { error } from 'console';
 import { stat } from 'fs';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -118,5 +120,21 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database Error: Failed to Delete Invoice.',
     };
+  }
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
   }
 }
